409ed97296366190037b9e810984119c887915c8
Student <user@chromebox-001.(none)>
2013-04-02 20:10:22 -0700

    end of day 2

--- model.py
+++ model.py
@@ -17,11 +17,11 @@ class User(Base):
 	age = Column(Integer, nullable=True)
 	zipcode = Column(String(15), nullable=True)
 
-	def __init__(self, email = None, password = None, age=None, zipcode=None):
-		self.email = email
-		self.password = password
-		self.age = age
-		self.zipcode = zipcode
+	# def __init__(self, email = None, password = None, age=None, zipcode=None):
+	# 	self.email = email
+	# 	self.password = password
+	# 	self.age = age
+	# 	self.zipcode = zipcode
 
 class Movie(Base):
 	__tablename__ = "movies"
@@ -31,10 +31,10 @@ class Movie(Base):
 	released_at = Column(String(64), nullable=True)
 	imbd_url = Column(String(64), nullable=True)
 
-	def __init__(self, name = None, released_at = None, imbd_url=None):
-		self.name = name
-		self.released_at = released_at
-		self.imbd_url = imbd_url
+	# def __init__(self, name = None, released_at = None, imbd_url=None):
+	# 	self.name = name
+	# 	self.released_at = released_at
+	# 	self.imbd_url = imbd_url
 
 class Rating(Base):
 	__tablename__ = "ratings"
@@ -44,10 +44,10 @@ class Rating(Base):
 	user_id = Column(Integer, nullable=True)
 	rating = Column(Integer, nullable=True)
 
-	def __init__(self, movie_id = None, user_id = None, rating = None):
-		self.movie_id = movie_id
-		self.user_id = user_id
-		self.rating = rating
+	# def __init__(self, movie_id = None, user_id = None, rating = None):
+	# 	self.movie_id = movie_id
+	# 	self.user_id = user_id
+	# 	self.rating = rating
 
 ### End class declarations
 
--- seed.py
+++ seed.py
@@ -1,27 +1,54 @@
 import model
 import csv
+from datetime import datetime
 
-def load_users(session):
-    # use u.user
-    with open('u.user', 'rb') as f:
-        reader = csv.reader(f, delimiter='|', quoting=csv.QUOTE_NONE)
-        for row in reader:
-            print row
+# open a file - YAY!
+# read a line - YAY!
+# parse a line - YAY!
+# create an object
+# add the object to a session
+# commit
+# repeat until done
+
+def load_users(session):     
+    # use u.user   
+    with open('seed_data/u.user') as f:
+        reader = csv.reader(f, delimiter='|')  
+        for row in reader: 
+            user = model.User(age=row[1], zipcode=row[4], id=row[0])
+            session.add(user)  
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/u.item') as f:
+        reader = csv.reader(f, delimiter='|')
+        for row in reader:
+            movie = model.Movie(id=row[0], released_at=row[2], imbd_url=row[4])
+            formatted_title = row[1]
+            formatted_title = formatted_title.decode("latin-1")
+            movie.name = formatted_title
+            # date_string = row[2]
+            # split date_string
+            # date_string = datetime.strptime(%s= , format)
+            session.add(movie)
 
 def load_ratings(session):
     # use u.data
-    pass
+    with open('seed_data/u.data') as f:
+        dialect = csv.Sniffer().sniff(f.read(1024))
+        f.seek(0)
+        reader = csv.reader(f, dialect)
+        for row in reader:
+            rating = model.Rating(movie_id=row[1], user_id=row[0], rating=row[2])
+            session.add(rating)
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    load_movies(session)
+    load_ratings(session)
+    session.commit()
 
 if __name__ == "__main__":
     s= model.connect()
     main(s)
-
-load_users
